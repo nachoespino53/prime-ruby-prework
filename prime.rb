169adb718e9ff8587447762fcd730a259f5fbe1f
Ignacio R Espino <irespino@capozios.com>
2018-08-22 20:41:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(n)
-  if n <= 1
+def prime?(number)
+  if number <= 1
     return false
   end
-  for d in 2..(n - 1)
-   if (n % d) == 0
+  for i in 2..(number - 1)
+   if (number % i) == 0
     return false
    end
   end
